7d33f18bdd04faa32b5cf6c5e1537d9c6afa92e6
sgnl <rayrfarias@gmail.com>
2016-03-11 16:35:22 -1000

    adds tests for presidents array

---
+++ tests/loops.spec.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+const Test = require('tape');
+const Util = require('util');
+const Fs = require('fs');
+const Vm = require('vm');
+const Path = require('path');
+
+let filePath = Path.resolve(__dirname, './../loops.js');
+const IndexFileRaw = Fs.readFileSync( filePath, { encoding: 'utf8' });
+
+let sandbox = {};
+const Script = new Vm.Script(IndexFileRaw, { filename: 'loops.js' });
+Script.runInNewContext(sandbox);
+
+Test('Getting Loopy', suite => {
+  Test('Presidents array', t => {
+    if (!sandbox.presidents) {
+      t.fail('presidents array is not defined.');
+      return t.end();
+    }
+
+    t.notEquals(sandbox.presidents, void 0, 'presidents array exists.')
+
+    let presidentsLastName = [ 'Washington'
+    , 'Adams'
+    , 'Jefferson'
+    , 'Madison'
+    , 'Monroe'
+    ];
+
+    presidentsLastName.forEach(name => {
+      t.ok(!!~sandbox.presidents.indexOf(name), `${name} is the last name of a former US president.`);
+    });
+
+    t.end();
+  });
+
+  suite.end();
+})
