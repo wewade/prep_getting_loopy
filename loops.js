1cf01f815469bd2361eb21dc0fda7d5400dd5118
wewade <wewade1506@gmail.com>
2016-09-14 18:41:55 -1000

    loops

--- loops.js
+++ loops.js
@@ -1,8 +1,11 @@
 /*## `Presidents - Accessing values in an Array`
 Declare a variable named `presidents` which contains the first 5 presidents' last names: `Washington`, `Adams`, `Jefferson`, `Madison`, `Monroe`.*/
-
+	
 /*Now write a _FOR_ loop which iterates through this Array.
+	
+	
 
+	
 Within the _FOR_ loop, use `console.log` to:
 
 - Log the value of `i`. Use a message like `Value of i is: ` and append the value of `i` to this String
@@ -10,26 +13,60 @@ Within the _FOR_ loop, use `console.log` to:
 
 /***bonus:** could you take the code inside of this FOR loop and encapsulate it inside of a function called `printContent` and still achieve the same result? This new function should accept two parameters.*/
 
+	var presidents = [ "Washington" , "Adams" , "Jefferson" , "Madison" , "Monroe"];	
+	for (var i = 0; i<presidents.length; i++) {
+		console.log(presidents[i]);
+		console.log("Value of i is: " + [i]);
+		console.log("Value at" , + [i] , + "is", + (presidents[i]));
+	}
 
+	function printContent(names){
+		for (var i = 0; i <names.length; i++) {
+		console.log("Value of i is:" [i]);
+		console.log("Value at" , + [i] , + "is" , (names[i]));
+		}
+		return names;
+	}
 
-
-
+		printContent(presidents);
 /*Declare a variable named `stringOfNumbers` and set it's value to `''` (an empty String).*/
 
 /*Then write a _FOR_ loop that appends a Number value to that string starting from `10` all the way up to and including `20`.*/
+		var stringOfNumbers = "";
+
+	for(var i = 10; i <21; i++){
+		stringOfNumbers += i;
+			}
+			console.log(stringOfNumbers);
 
 
 
 /***bonus:** could you take the code inside of this _FOR_ loop and encapsulate it inside of a function called `appendToString` and still achieve the same result?*/
+	
+			var numberstrng = "";
 
-
+			function appendtoString(strng){
+				for (var i =10; i <21; i++){
+					strng += i;
+				}					
+					return strng;
+			}
+			
+				console.log(appendtoString(numberstrng));
 
 /*## `Add only even numbrs to an array`
 Declare a variable named `evenNumberArray`.
 
 Use a _FOR_ loop to add only even numbers to an Array. Add `50` even numbers to the `evenNumberArray` starting with the value `0`.*/
 
-  
+  var evenNumberArray =[];
+  for (var i =0; i<99; i++) {
+  	if(i%2===0){
+  		evenNumberArray.push(i);
+  	}
+  }
+  console.log(evenNumberArray);
+
 
 /*## `Accessing only the odd indexes of an Array - 'Not Even Brah'`
 Someone forgot to fill out this array! Oh noes...
@@ -43,7 +80,14 @@ Example result should look like:
 ```javascript
 `[ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]`*/
 
-
+	var oopsArray =["turn" ,,"down",, "for" ,, "what"];
+		for (var i = 0; i <oopsArray.length; i++) {
+			if(i%2===1){
+				oopsArray[i]= "nope";
+			}
+		
+		}
+			console.log(oopsArray);
 
 /*## `Going backwards?!`
 Using a _FOR_ loop, iterate through the Array stored at `oopsArray` **backwards**. `console.log` each value in the Array.
@@ -59,8 +103,12 @@ nope
 turn
 ```*/
 
+	var oopsArrayBackwards = [];
+		for (var i = oopsArray.length -1; i>=0; i--){
+		 oopsArrayBackwards.push(oopsArray);
 
-
+		}
+				console.log(oopsArrayBackwards);
 
 /*## `isNapTime`
 Declare a variable named `isNapTime`. Set it to `false`
@@ -74,6 +122,28 @@ Declare a function named `nap`. This function takes in a single parameter called
 
 Now, Write a FOR loop that iterates through the `napSchedule` array and runs the function `nap` while passing in the value at the current position of `napSchedule` into the `nap` function.*/
 
+var isNapTime = false;
+var napSchedule =[false , false , true , false , true , true];
+
+function nap(schedule){
+	if(schedule===true){
+		console.log ("ZzZzZzZz");
+
+	}else{
+		console.log("Gotta get to work!");
+			isNapTime = true;
+	}
+	}
+		for (var i = 0; i<napSchedule.length; i++){
+		nap(napSchedule[i]);
+
+		}
+			
+			
+						
+			
+			
+
 
 
 
@@ -82,7 +152,17 @@ Now, Write a FOR loop that iterates through the `napSchedule` array and runs the
 Declare a function named `copyArray` which takes two arguments: `originArray` and `destinationArray`. Inside of this function you will loop through the contents of `originArray` and push each value into `destinationArray`.
 
 To get started, below your function declaration, call your function and pass in the two variables, `valuesArray` and `copyOfValuesArray`. After that, use `console.log` to to inspect the values of `valuesArray` and `copyOfValuesArray` to make sure they have the same values (which means your function worked!).*/
+ var valuesArray =[99 , 66 , 829, 1941, 8 , 76];
+ var copyOfValuesArray =[];
 
+ function copyArray(originArray , destinationArray){
+ 	for (var i = 0; i<originArray.length; i++){
+ 		destinationArray.push(originArray[i]);
+
+ 		}
+ 		return destinationArray;
+ 	}
+ 	 console.log(copyArray(valuesArray , copyOfValuesArray));
 
 
 
@@ -91,6 +171,8 @@ To get started, below your function declaration, call your function and pass in 
 
 ## Stage 1 - Only String Values
 Declare a variable named `miscStorage` set it's value to be: `[ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ]`
+
+
 
 Declare a function named `generateArrayOfStrings` which takes a single argument `storage`. This function returns a new Array with only `String` values inside of it.
 
@@ -105,4 +187,19 @@ Your function will iterate through the `class` argument and check each student's
 
 If the `enrolled` property is set to `true` then change that student's `graduated` property to `true`. Otherwise, if `enrolled` is set to `false` then change `enrolled` to `true` leaving `graduated` alone and unchanged.
 */
+
+var miscStorage =[ [] ,  " Carrots " , 9 , "Beets" , {} , {name: "Todd B."} , "Mush" ];
+function generateArrayOfStrings(storage){
+	return [];
+
+
+}
+
+var currentClass =[array found here](https://
+	function graduateAndSetNewClass(class){
+
+		
+	}
+
+
 
